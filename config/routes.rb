fa4439155bdb9148f319affbf3cb750d6976d3e9
EC2 Default User <ec2-user@ip-172-31-93-84.ec2.internal>
2021-04-04 10:47:12 +0000

    ouyou4

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
-    resource :favorites, only: [:create, :destroy]
-    resources :book_comments, only: [:create, :destroy]
-  end
+   resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+ end
+  resources :users, only: [:index, :show, :edit, :update]do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+ end
 end
